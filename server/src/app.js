444899b35ced43cd342bfd28cf8d8b574ea3da31
Shameer s n <shameersalnaz@gmail.com>
2019-08-08 06:20:52 +0530

    Added sample register route

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,22 @@
-console.log("Hi Node");
+const PORT = process.env.PORT || 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+  res.send({ message: `Hello there ${req.body.email}` });
+});
+
+app.use("**", (req, res) => {
+  res.json(`Server is up ${new Date()}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Express app running on ${PORT}`);
+});
